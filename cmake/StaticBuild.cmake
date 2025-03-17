c1f08d1ec02ca5d64e869255005e173f01183090
ArqTras <biuro@ar-net.info>
2025-03-17 01:28:08 +0100

    boost

--- cmake/StaticBuild.cmake
+++ cmake/StaticBuild.cmake
@@ -7,7 +7,7 @@ set(OPENSSL_HASH SHA256=892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df
 
 set(BOOST_VERSION 1.76.0 CACHE STRING "boost version")
 string(REPLACE "." "_" BOOST_VERSION_ ${BOOST_VERSION})
-# Ustawienie głównego źródła na SourceForge
+# Poprawiony link do SourceForge
 set(BOOST_MIRROR "https://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION}/boost_${BOOST_VERSION_}.tar.bz2/download"
                  CACHE STRING "boost download mirror(s)")
 set(BOOST_SOURCE boost_${BOOST_VERSION_}.tar.bz2)
@@ -15,7 +15,6 @@ set(BOOST_HASH SHA256=f0397ba6e982c4450f27bf32a2a83292aba035b827a5623a14636ea583
 # Debugging - wypisanie ustawionych wartości
 message(STATUS "Downloading Boost from: ${BOOST_MIRROR}")
 message(STATUS "Expected hash: ${BOOST_HASH}")
-
 
 set(SODIUM_VERSION 1.0.18 CACHE STRING "libsodium version")
 set(SODIUM_MIRROR ${LOCAL_MIRROR} https://download.libsodium.org/libsodium/releases https://github.com/jedisct1/libsodium/releases/download/${SODIUM_VERSION}-RELEASE CACHE STRING "libsodium mirror(s)")
